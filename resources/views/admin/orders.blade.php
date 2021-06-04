8c5a7dc6b5d3e27607d5f29dd12fbed356c3f15c
andrawicaksono <andrawicaksono@apps.ipb.ac.id>
2021-06-04 11:00:47 +0700

    Create orders.blade.php

---
+++ resources/views/admin/orders.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.adminapp')
+
+@section('title')
+
+<title>Orders - Admin Kopikimo</title>
+    
+@endsection
+
+@section('content')
+
+<h1 class="text-center">Orders</h1>
+    
+@endsection
