cbc9af1b0337a4e377474e15ec835dbe48586819
andrawicaksono <andrawicaksono@apps.ipb.ac.id>
2021-06-05 00:11:09 +0700

    minor fix

--- resources/views/admin/products.blade.php
+++ resources/views/admin/products.blade.php
@@ -3,47 +3,48 @@
 @section('title')
 
 <title>Products - Admin Kopikimo</title>
-
+    
 @endsection
 
 @section('content')
 
-<style>
-    th, td {
-        border: 1px solid black;
-    }
-</style>
-
 <h1 class="text-center">Products</h1>
 
-
-
-<table style="width:100%">
-    <tr>
-        <th>Product ID</th>
-        <th>Nama Kopi</th>
-        <th>Kemasan</th>
-        <th>Ukuran</th>
-        <th>Extrashot</th>
-        <th>Stock</th>
-        <th>Harga</th>
-    </tr>
-    @foreach ($products as $item)
-    <tr>
-        <td>{{ $item->id }}</td>
-        <td>{{ $coffees->where('id', $item->coffee_id)->first()->nama }}</td>
-        <td>{{ $packs->where('id', $item->pack_id)->first()->nama }}</td>
-        <td>{{ $sizes->where('id', $item->size_id)->first()->nama }}</td>
-        <td>
-            @if ($item->extrashot)
-                Pake
-            @else
-                Ngga Pake
-            @endif
-        </td>
-        <td>{{ $item->stock }}</td>
-        <td>{{ $item->harga }}</td>
-    </tr>
-    @endforeach
-</table>
+<div id="container" class="text-center">
+    <table style="width: 100%">
+    
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Pack</th>
+            <th>Size</th>
+            <th>Extrashot</th>
+            <th>Stock</th>
+            <th>Price</th>
+            <th></th>
+        </tr>
+    
+        
+        @foreach ($products as $item)
+            <tr>
+                <td>{{ $item->id }}</td>
+                <td>{{ $coffees->where('id', $item->coffee_id)->first()->nama }}</td>
+                <td>{{ $packs->where('id', $item->pack_id)->first()->nama }}</td>
+                <td>{{ $sizes->where('id', $item->size_id)->first()->nama }}</td>
+                <td>
+                    @if ($item->extrashot)
+                        Yes
+                    @else
+                        No
+                @endif
+                </td>
+                <td>{{ $item->stock }}</td>
+                <td>{{ $item->harga }}</td>
+                <td><a href="/admin/products/{{ $item->id }}"><button>Edit</button></a></td>
+            </tr>
+        @endforeach
+        
+    </table>
+    </div>
+    
 @endsection
