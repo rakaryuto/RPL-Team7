97429bfa157409d563e535d209eabb87c94452fc
andrawicaksono <andrawicaksono@apps.ipb.ac.id>
2021-06-04 10:59:49 +0700

    Create products.blade.php

---
+++ resources/views/admin/products.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.adminapp')
+
+@section('title')
+
+<title>Products - Admin Kopikimo</title>
+    
+@endsection
+
+@section('content')
+
+<h1 class="text-center">Products</h1>
+    
+@endsection
