ced6a2121dfa8568939d49b8fbf80863d08d0968
andrawicaksono <andrawicaksono@apps.ipb.ac.id>
2021-06-05 00:53:00 +0700

    fix Description input and fix layout

--- resources/views/admin/edit.blade.php
+++ resources/views/admin/edit.blade.php
@@ -11,25 +11,25 @@
 
 <h1 class="text-center">Edit Products</h1>
 
-<form method="post">
+<form method="post" style="width: 100%">
     @csrf
 
     <input type="hidden" name="id" value="{{$item->id}}">
 
     <div>
-        <label for="name">Name</label>
+        <label for="name">Name</label><br>
         <input type="text" name="name" id="name" required value="{{ $coffees->where('id', $item->coffee_id)->first()->nama }}">
     </div> <br>
     <div>
-        <label for="desc">Description</label>
-        <input type="text" name="desc" id="desc" value="{{ $coffees->where('id', $item->coffee_id)->first()->deskripsi }}">
+        <label for="desc">Description</label><br>
+        <textarea name="desc" id="desc" style="width: 50%">{{ $coffees->where('id', $item->coffee_id)->first()->deskripsi }}</textarea>
     </div><br>
     <div>
-        <label for="stock">Stocks</label>
+        <label for="stock">Stocks</label><br>
         <input type="number" name="stocks" id="stocks" required value="{{$item->stock}}">
     </div><br>
     <div>
-        <label for="price">Price</label>
+        <label for="price">Price</label><br>
         <input type="number" name="price" id="price" required value="{{$item->harga}}">
     </div><br>
     <div>
